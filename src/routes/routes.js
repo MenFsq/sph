e01a2951c9ebe73e8a32369dfdeb15dce7e03d86
PUDDIN <www.493319204@qq.com>
2020-12-15 10:43:50 +0800

    '项目初始化'

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,4 @@
-import test from 'pages/test'
 const routes = [
-    { path: '/test', component: test },
+
 ];
 export default routes;
--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,12 @@ import state from './state'
 import getters from './getters'
 import mutations from './mutations'
 import actions from './actions'
-import house from './modules/house'
 const store = new Vuex.Store({
     state,
     getters,
     mutations,
     actions,
     modules: {
-        house,
     }
 })
 export default store;
